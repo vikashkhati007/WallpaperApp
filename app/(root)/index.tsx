31c4174eaf7e4b1cb7d88173fc0a12500486a102
Vikash Khati <vikashkhati007@gmail.com>
2024-10-05 16:42:27 +0530

    react navigation added...

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import Library from '@/components/Home/library';
+import Liked from '@/components/Home/liked';
+import Suggested from '@/components/Home/suggested';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+const Tab = createMaterialTopTabNavigator();
 
 export default function index(){
     return (
-        <View>
-            <Text>Index</Text>
-        </View>
+    <Tab.Navigator>
+        <Tab.Screen name="Suggested" component={Suggested} />
+        <Tab.Screen name="Liked" component={Liked} />
+        <Tab.Screen name="Library" component={Library} />
+    </Tab.Navigator>
     )
 }
+
---
+++ components/Home/suggested.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Suggested() {
+  return (
+    <View>
+      <Text>Suggested</Text>
+    </View>
+  );
+}
